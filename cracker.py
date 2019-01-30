0ac7ab5502cf489044641194410b5051f26d0df6
Austin <austin@localhost.localdomain>
2019-01-29 19:38:06 -0500

    Added comments to cracker.py file

---
+++ cracker.py
@@ -0,0 +1,21 @@
+##Rules for passwords to have##
+##
+##  1) 7 character word from /usr/share/dict/words
+##     gets the 1st letter capitalized & a 1-digit
+##     number appended
+##
+##  2) 10 digit password with at least one of the
+##     following special characters in the 
+##     beginning: *, ~, !, #
+##
+##  3) 5 character word from /usr/share/dict/words
+##     with the letter 'a' in it which gets
+##     replaced with any special character @ and 
+##     the character 'l' is substituted by the 
+##     number '1'. 
+##
+##  4) Any word that is made with digits up to
+##     100 digits length
+##
+##  5) Any number of characters single word from
+##     /usr/share/dict/words
