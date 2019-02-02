54ca2fa0456ab32b58a50faccf8276264b3cad25
root <root@localhost.localdomain>
2019-02-01 22:37:02 -0500

    Added threads, main function, and getCracked function

--- cracker.py
+++ cracker.py
@@ -1,4 +1,5 @@
 import hashlib
+import threading
 
 ##Rules for passwords to have##
 ##
@@ -40,44 +41,70 @@ import hashlib
 
 ## Practice and testing how to compare hashes.
 ## Update works may be onto something.
+def main():
+    ## Grab the Text File With Passwords to Decrypt
+    print("Please enter the text file with the hashed passwords: ")
+    textFile = input()
+    print("Reading in the file ", textFile)
 
-## Grab the Text File With Passwords to Decrypt
-print("Please enter the text file with the hashed passwords: ")
-textFile = input()
-print("Reading in the file ", textFile)
+    ## Read in the Text File with Passwords into a variable.
+    hashedFile = open(textFile, "r")
+    ## Split the file into lines denoted by new line characters
+    hashedLines = hashedFile.read().split("\n")
 
-## Read in the Text File with Passwords into a variable.
-hashedFile = open(textFile, "r")
-## Split the file into lines denoted by new line characters
-hashedLines = hashedFile.read().split("\n")
+    ## Printing for accuracy
+    for line in hashedLines:
+        #print(line)
+        a =0 #deletelater
+    print("Finished reading in ", textFile)
+    ## Close the file
+    hashedFile.close()
 
-## Printing for accuracy
-for line in hashedLines:
-    print(line)
-print("Finished reading in ", textFile)
-## Close the file
-hashedFile.close()
+    ## Parse the encrypted password file here for later use
+    ## Split at the ":" character; use join to join it together
+    ## after finding the hashed password.
+    userArr = []
+    hashedPassArr = []
+    ## Create separate arrays for Users and Hashed Passwords to be passed into threading function
+    for line in hashedLines:
+        seq = line.split(":")
+        user , hashedPass , rest = seq[0],seq[1], seq[2:]
+        userArr.append(user)
+        hashedPassArr.append(hashedPass)
+    
+    
+    threads = []
+    i = 0
+    for i in range(len(userArr)):
+        t = threading.Thread(target = getCracked, args = (userArr[i], hashedPassArr[i]))
+        threads.append(t)
+        t.start()
+    #getCracked(userArr[0], hashedPassArr[0])
+    #getCracked(userArr[1], hashedPassArr[1])
+    #getCracked(userArr[2], hashedPassArr[2])
+        
 
-## Parse the encrypted password file here for later use
-## Split at the ":" character; use join to join it together
-## after finding the hashed password.
 
+#function to run all rules (passed into each thread)
+def getCracked(username, hashedPassword):
+    ## Open the text file for the wordlist
+    currUser = username
+    currHashedPassword = hashedPassword
+    print("The user is: " + currUser + " Their Password is: " + currHashedPassword + "\n")
+    #print("Reading in text file /usr/share/dict/words...")
+    f = open("/usr/share/dict/words", "r")
+    ## Read the file and split on space
+    words = f.read().split()
+    ## Close the file
+    f.close()
+    #print("Finished reading in text file /usr/share/dict/words...")
 
-## Open the text file for the wordlist
-print("Reading in text file /usr/share/dict/words...")
-f = open("/usr/share/dict/words", "r")
-## Read the file and split on space
-words = f.read().split()
-## Close the file
-f.close()
-print("Finished reading in text file /usr/share/dict/words...")
+    ## Remove later used for testing the parsing of the file
+    ## and formatting everything to required formats
+    exit()
 
-## Remove later used for testing the parsing of the file
-## and formatting everything to required formats
-exit()
-
-#m = hashlib.sha256()
-#print(list(words[102400]))
+    #m = hashlib.sha256()
+    #print(list(words[102400]))
 
 
 ############# TO DO ###############
@@ -94,71 +121,73 @@ exit()
 
 
 ## decide how the threads will guess which rule to use
-for i in range(len(words) - 1):
+    for i in range(len(words) - 1):
 
-## Testing for one iteration Remove later
-#for i in range(1):
+    ## Testing for one iteration Remove later
+    #for i in range(1):
 
-    ## Reset the hash for every new word tested
-    m = hashlib.sha256()
+        ## Reset the hash for every new word tested
+        m = hashlib.sha256()
 
-    ## This is the rule for 7 character word with one 
-    ## digit appended to the end of the word
-    if((len(words[i])) == 7):
+        ## This is the rule for 7 character word with one 
+        ## digit appended to the end of the word
+        if((len(words[i])) == 7):
 
-        #print(words[i])
-        newWord = words[i].capitalize()
-        for j in range(10):
-            ## Reset the hash for every updated
-            ## run involved a new number added to end
-            m = hashlib.sha256()
-            
-            ## Add a number between and including 0-9
-            ## to the end of the word being tested.
-            newWord += str(j)
-
-            ## Encode the word because Hashlib said so
-            encodedWord = newWord.encode('utf-8')
-
-            ## Testing the word being used with added digit
-            #print(newWord)
-
-            ## Testing portion Remove later
-            #ultraword = "Puzzles42"
-            #print(ultraword)
-
-            ## Update the word being hashed and compared to
-            ## the hashed password in the file
-            hashed = m.update(encodedWord)
-
-            ## Testing purposes for single use for accurate results
-            #hashed = m.update(ultraword.encode('utf-8'))
-            #string = "c1df467d16a2ebea8b48482c10d6c640e163c69f5da3dfba4c442002e5e15fa9"
-            
-            ## Digest the hashed word using hexdigest()
-            hashed = m.hexdigest()
-
-            ## Test the hashed word for visual confirmation
-            print(hashed)
-
-            ## If the hashed password matches the string
-            ## that was recovered from the user entered file
-            ## then the password has been found.
-            if(hashed == string):
-                ##Testing statements Remove later##
-                print("Password Should be: Atari/'s2\n")
-                #print("Hashes equal each other")
-                print("Password is ", newWord)
-
-
-                ## Close the thread after outputting the
-                ## results to a file in the correct format
-                exit()
-            
-            ## Testing statement
-            #if(ultraword == "Puzzles42"):
-            #    exit()
-            
-            ## Reset the word back to original
-            ## word without digits appended to it
+            #print(words[i])
             newWord = words[i].capitalize()
+            for j in range(10):
+                ## Reset the hash for every updated
+                ## run involved a new number added to end
+                m = hashlib.sha256()
+            
+                ## Add a number between and including 0-9
+                ## to the end of the word being tested.
+                newWord += str(j)
+
+                ## Encode the word because Hashlib said so
+                encodedWord = newWord.encode('utf-8')
+
+                ## Testing the word being used with added digit
+                #print(newWord)
+
+                ## Testing portion Remove later
+                #ultraword = "Puzzles42"
+                #print(ultraword)
+
+                ## Update the word being hashed and compared to
+                ## the hashed password in the file
+                hashed = m.update(encodedWord)
+
+                ## Testing purposes for single use for accurate results
+                #hashed = m.update(ultraword.encode('utf-8'))
+                #string = "c1df467d16a2ebea8b48482c10d6c640e163c69f5da3dfba4c442002e5e15fa9"
+            
+                ## Digest the hashed word using hexdigest()
+                hashed = m.hexdigest()
+
+                ## Test the hashed word for visual confirmation
+                print(hashed)
+
+                ## If the hashed password matches the string
+                ## that was recovered from the user entered file
+                ## then the password has been found.
+                if(hashed == string):
+                    ##Testing statements Remove later##
+                    print("Password Should be: Atari/'s2\n")
+                    #print("Hashes equal each other")
+                    print("Password is ", newWord)
+
+
+                    ## Close the thread after outputting the
+                    ## results to a file in the correct format
+                    exit()
+            
+                ## Testing statement
+                #if(ultraword == "Puzzles42"):
+                #    exit()
+            
+                ## Reset the word back to original
+                ## word without digits appended to it
+                newWord = words[i].capitalize()
+
+main()
