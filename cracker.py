82f00ea71aef4c425a60efd64ff7615ebb5bcfa0
hollisas <hollisas@g.cofc.edu>
2019-02-02 19:26:16 -0500

    Updated format of comments

--- cracker.py
+++ cracker.py
@@ -1,43 +1,23 @@
 import hashlib
 import threading
 
-##Rules for passwords to have##
-##
-##  1) 7 character word from /usr/share/dict/words
-##     gets the 1st letter capitalized & a 1-digit
-##     number appended
-##
-##  2) 10 digit password with at least one of the
-##     following special characters in the 
-##     beginning: *, ~, !, #
-##
-##  3) 5 character word from /usr/share/dict/words
-##     with the letter 'a' in it which gets
-##     replaced with any special character @ and 
-##     the character 'l' is substituted by the 
-##     number '1'. 
-##
-##  4) Any number that is made with digits up to
-##     10 digits length
-##
-##  5) Any number of characters single word from
-##     /usr/share/dict/words
-
-## Potential setup:
-## Create a menu that will read user input and do guesses off of that? (This menu may be handled differently based on what Dr. X wants)
-## iterate through the list of words based off of rule and try each successful match to the rule.
-##  while(!cracked OR !NoMoreGuessesInFile)
-##      If EndOfFileCharacter (Essentially NoMoreGuessesInFile)
-##          Update the !NoMoreGuessesInFile variable to kick out of while loop
-##      Else If word does not match go to next word
-##          Find the next matching word
-##      else hash that word then compare that to the hash portion in the line
-##          If hashes match update !cracked to kick out of while loop
-##  If(cracked)
-##      output password
-##  Else
-##      Ask the user if they want to run a different rule set? (Based on requirements)
-##      
+##########################################################################
+##                     Rules for passwords to have                      ##
+##                                                                      ##
+##  1) 7 character word from /usr/share/dict/words gets the 1st letter  ##
+##     capitalized & a 1-digit number appended                          ##
+##                                                                      ##
+##  2) 10 digit password with at least one of the following special     ##
+##     characters in the beginning: *, ~, !, #                          ##
+##                                                                      ##
+##  3) 5 character word from /usr/share/dict/words with the letter 'a'  ##
+##     in it which gets replaced with any special character @ and the   ##
+##     character 'l' is substituted by the number '1'.                  ##
+##                                                                      ##
+##  4) Any number that is made with digits up to 10 digits length       ##
+##                                                                      ##
+##  5) Any number of characters single word from /usr/share/dict/words  ##
+##########################################################################
 
 ## Practice and testing how to compare hashes.
 ## Update works may be onto something.
@@ -119,7 +99,7 @@ def getCracked(username, hashedPassword):
 ## gets the 1st letter capitalized & a 1-digit  ##
 ## number appended                              ##
 ##################################################
-def rule1(user, hashedPass):
+def rule1(user, password):
     print("Reading in text file /usr/share/dict/words...")
     
     ## Read in the dictionary of words
@@ -160,15 +140,15 @@ def rule1(user, hashedPass):
 
                 ## Update the word being hashed and compared to
                 ## the hashed password in the file
-                hashed = m.update(encodedWord)
+                m.update(encodedWord)
             
                 ## Digest the hashed word using hexdigest()
-                hashed = m.hexdigest()
+                hashedWord = m.hexdigest()
 
                 ## If the hashed password matches the string
                 ## that was recovered from the user entered file
                 ## then the password has been found.
-                if(hashed == hashedPass):
+                if(hashedWord == password):
                     ##### Write to file??? ####
                     print("Password is ", newWord)
 
