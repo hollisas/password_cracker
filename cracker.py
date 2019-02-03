c0809fcb4f3e77d2fc4261ee03490235a3bbc3ed
hollisas <hollisas@g.cofc.edu>
2019-02-02 21:22:02 -0500

    Updated comments throughout

--- cracker.py
+++ cracker.py
@@ -7,14 +7,14 @@ import threading
 ##  1) 7 character word from /usr/share/dict/words gets the 1st letter  ##
 ##     capitalized & a 1-digit number appended                          ##
 ##                                                                      ##
-##  2) 10 digit password with at least one of the following special     ##
+##  2) 4 digit password with at least one of the following special      ##
 ##     characters in the beginning: *, ~, !, #                          ##
 ##                                                                      ##
 ##  3) 5 character word from /usr/share/dict/words with the letter 'a'  ##
 ##     in it which gets replaced with any special character @ and the   ##
 ##     character 'l' is substituted by the number '1'.                  ##
 ##                                                                      ##
-##  4) Any number that is made with digits up to 10 digits length       ##
+##  4) Any number that is made with digits up to 6 digits length        ##
 ##                                                                      ##
 ##  5) Any number of characters single word from /usr/share/dict/words  ##
 ##########################################################################
@@ -152,7 +152,7 @@ def rule1(user, password, threadNum):
 
 ##################################################
 ## Rule 2:                                      ##
-## 10 digit password with at least one of the   ##
+## 4 digit password with at least one of the   ##
 ## following special characters in the          ##
 ## beginning: '*', '~', '!', '#'                ##
 ##################################################
@@ -224,7 +224,7 @@ def rule3(user, password, threadNum):
 ##################################################
 ## Rule 4:                                      ##
 ## Any number that is made with digits up to    ##
-## 10 digits length                             ##
+## 6 digits length                              ##
 ##################################################
 def rule4(user, password, threadNum):
     ## Read in txt file that was created and submitted
